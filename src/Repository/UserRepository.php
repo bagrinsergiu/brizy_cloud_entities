22d82444dddeac457ff89c85ec5113512658c3d0
Zaharia Alexandru <alecszaharia@gmail.com>
2025-02-26 15:26:08 +0200

    composer update..

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,7 +8,6 @@ use Brizy\Bundle\CloudEntitiesBundle\Entity\User;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\Persistence\ManagerRegistry;
-use Trikoder\Bundle\OAuth2Bundle\Model\AccessToken;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -23,45 +22,4 @@ class UserRepository extends EntityRepository
         parent::__construct($registry, User::class);
     }
 
-    public function getUserIdClientIdentifier($clientId)
-    {
-        $result = $this->createQueryBuilder('u')
-                       ->select('u.id')
-                       ->where('u.cms_api_client=:client and u.locked=false')
-                       ->setParameter('client', $clientId)
-                       ->getQuery()->getOneOrNullResult(AbstractQuery::HYDRATE_ARRAY);
-
-        if ($result) {
-            return $result['id'];
-        }
-    }
-
-    public function isProjectAllowedForUser($projectId, $userId)
-    {
-        // this logic is on cloud site for now.
-
-        // return true  if the user exist
-        return (bool) $this->createQueryBuilder('u')
-                          ->select('u.id')
-                          ->where('u.id=:user_id and u.locked=false')
-                          ->setParameter('user_id', $userId)
-                          ->getQuery()->getOneOrNullResult(AbstractQuery::HYDRATE_ARRAY);
-    }
-
-    public function getUserByAccessTokenIdentifier(string $identifier): ?User
-    {
-        $result = $this->createQueryBuilder('u')
-            ->join(
-                AccessToken::class,
-                'at',
-                'WITH',
-                'at.identifier=:identifier'
-            )
-            ->setParameter('identifier', $identifier, Types::STRING)
-            ->where('u.cms_api_client = at.client')
-            ->getQuery()
-            ->getOneOrNullResult(AbstractQuery::HYDRATE_OBJECT);
-
-        return $result;
-    }
 }
