d328497f20c4aaa885fe50b8ba4e67f0cde28852
Zaharia Alexandru <alecszaharia@gmail.com>
2025-02-26 15:24:04 +0200

    composer update..

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -413,9 +413,8 @@ class User implements UserInterface
      * This is important if, at any given point, sensitive information like
      * the plain-text password is stored on this object.
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
-        return null;
     }
 
     public function __call($name, $arguments)
